39ade906de6e5b59708245fa0119f0a68e7d2296
Pavel Radkevich <pradkevich@elinext.com>
2019-12-11 12:58:13 +0300

    completed deadlock_DDL exercise + study drills

--- src/test/java/part_01_jdbc/code/DeadlockDDLExercise.java
+++ src/test/java/part_01_jdbc/code/DeadlockDDLExercise.java
@@ -1,11 +1,13 @@
 package part_01_jdbc.code;
 
 import org.h2.jdbc.JdbcSQLException;
+import org.h2.jdbc.JdbcSQLTimeoutException;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DeadlockDDLExercise {
@@ -19,21 +21,32 @@ public class DeadlockDDLExercise {
         }
     }
 
-    @Test(expected = JdbcSQLException.class)
+    @Test(expected = JdbcSQLTimeoutException.class)
     public void deadlockDDL_exercise() throws SQLException {
         System.out.println("Do we reach the end of the test?...(No, if deadlock occurs)");
         try (Connection connectionPR = getConnection()) {
             connectionPR.setAutoCommit(false);
             connectionPR.createStatement().execute("insert into items" +
                     "(name) values ('CTU Field Report')");
-            try (Connection connectionKV = getConnection()){
+            try (Connection connectionKV = getConnection()) {
                 connectionKV.setAutoCommit(false);
-                connectionKV.createStatement().execute("alter table items add column (release_date date null)");
+                //connectionKV.createStatement().execute("alter table items add column (release_date date null)");
+                //connectionKV.createStatement().execute("drop table items");
+                //connectionKV.createStatement().execute("alter table items rename to test_items");
+                connectionKV.createStatement().execute("truncate table items");
                 connectionKV.commit();
             }
             connectionPR.commit();
         }
         System.out.println("Yes!");
+
+        try (Connection checkConnection = getConnection()){
+            ResultSet resultSet = checkConnection.createStatement()
+                    .executeQuery("select * from test_items");
+            resultSet.next();
+            System.out.println("Name from DB: " + resultSet.getString("name"));
+        }
+
     }
 
     private Connection getConnection() throws SQLException {
